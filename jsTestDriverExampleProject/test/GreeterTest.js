dd64bdb99f27023f6888128d30ae54eb23131752
Dave <david.bethke@gmail.com>
2011-07-28 14:15:02 -0500

    try orig test again

--- jsTestDriverExampleProject/test/GreeterTest.js
+++ jsTestDriverExampleProject/test/GreeterTest.js
@@ -26,17 +26,19 @@ checkNumbersOnlyTest=TestCase("checkNumbersOnlyTest");
 
 checkNumbersOnlyTest.prototype.testCheckNumbersOnlyTest= function(){
 	
-	//	var text=document.createTextNode('123456');
-		//var element=document.createElement('div');
-		//element.id='testElement';
-		//element.appendChild(text);
+		var text=document.createTextNode('123456');
+		var element=document.createElement('div');
+		element.id='testElement';
+		element.appendChild(text);
+		/*
 		div = document.createElement('div');
 		div.setAttribute('id', 'foo');
 		  var p = document.createElement('p');
 		  div.appendChild(p);
 		  p.innerHTML = "bar";
 		  div.id = 'foo';
-		  document.body.appendChild(div);
+		  */
+		  document.body.appendChild(element);
 		  //$("p").add("div");
 		  /*:DOCfoo=<div><p>bar</p></div>*/
 		
@@ -44,8 +46,8 @@ checkNumbersOnlyTest.prototype.testCheckNumbersOnlyTest= function(){
 		//element.appendChild(text);
 		//document.body.appendChild(element);
 		var msg= 'this is a message';
-		assertNotNull(document.getElementById('foo'));
-		//assertTrue(checkNumbersOnly(element,msg));
+		//assertNotNull(document.getElementById('foo'));
+		assertTrue(checkNumbersOnly(element,msg));
 		
 };
 
